b82dc8ff516bd7601796def80d2c3f0757d6bdeb
飞鱼 <782944218@qq.com>
2020-02-17 00:07:47 +0800

    add check cookies

--- src/main/java/org/lzk/community/mapper/UserMapper.java
+++ src/main/java/org/lzk/community/mapper/UserMapper.java
@@ -2,6 +2,8 @@ package org.lzk.community.mapper;
 
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 import org.lzk.community.model.User;
 
 @Mapper
@@ -10,4 +12,6 @@ public interface  UserMapper {
     @Insert(" insert into t_user (name,account_id,token,gmt_create,gmt_modified) values (#{name},#{accountId},#{token},#{gmtCreate},#{gmtModified}) ")
     void insertUser(User user);
 
+    @Select(" select * from t_user where token = #{token} ")
+    User findByToken(@Param("token") String token);
 }
